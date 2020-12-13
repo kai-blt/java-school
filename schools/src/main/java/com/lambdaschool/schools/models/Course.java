00513a44801f9b004bddb54128656978a262f2af
kai-blt <blaitong@gmail.com>
2020-12-13 11:02:38 -0800

    added field validation for coursename

--- schools/src/main/java/com/lambdaschool/schools/models/Course.java
+++ schools/src/main/java/com/lambdaschool/schools/models/Course.java
@@ -3,6 +3,7 @@ package com.lambdaschool.schools.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,8 +26,9 @@ public class Course
     /**
      * Name (String) of this Course. Cannot be null and must be unique
      */
-    @Column(nullable = true,
-        unique = true)
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 2, max = 50, message="Course name must be between 2 and 50 characters")
     private String coursename;
 
     /**
